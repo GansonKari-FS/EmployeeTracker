72e5e177143d43436b58b004f8017e4eba8d078f
GansonKari-FS <kDganson@student.fullsail.edu>
2025-09-25 20:58:59 -0500

    added some variables to hold local storage
    to attempt and show the list of employees being tracked and to be able to show to the list from users input i added some variables and a function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // IIFE
 (function () {
   // Employee class
+  // dad class
   class Employee {
     constructor(name, age) {
       this.name = name;
@@ -10,8 +11,10 @@
   }
 
   // Part-Time class
+  // child class
   class PartTime extends Employee {
     constructor(name, age, payRate, hours) {
+      // super
       super(name, age);
       this.payRate = payRate;
       this.hours = hours;
@@ -28,6 +31,7 @@
   // Manager class
   class Manager extends Employee {
     constructor(name, age, payRate, hours) {
+      // super
       super(name, age);
       this.payRate = payRate;
       this.hours = hours;
@@ -46,17 +50,35 @@
     constructor() {
       // empty array to add employees to
       this.employees = [];
+      this.loadList();
 
-      // employees names list
-      this.employees.push(new PartTime("Jennifer Sanders", 25, 15, 10));
-      this.employees.push(new PartTime("Sally Johnson", 33, 12, 10));
-      this.employees.push(new PartTime("Jeff Ganson", 30, 17, 16));
-      this.employees.push(new Manager("Steve Thibodeaux", 40, 20, 15));
-      this.employees.push(new Manager("Brooke Barker", 20, 20, 15));
+      if (this.employees.length === 0) {
+        // list of inital employees names
+        this.employees.push(new PartTime("Jennifer Sanders", 25, 15, 10));
+        this.employees.push(new PartTime("Sally Johnson", 33, 12, 10));
+        this.employees.push(new PartTime("Jeff Ganson", 30, 17, 16));
+        this.employees.push(new Manager("Steve Thibodeaux", 40, 20, 15));
+        this.employees.push(new Manager("Brooke Barker", 20, 20, 15));
+        this.saveList();
+      }
 
       // call
-      this.displayEmployees();
+      //   this.displayEmployees();
       this.menu();
+    }
+
+    loadFromStorage() {
+      const saved = localStorage.getItem("employees");
+      if (saved) {
+        const rawEmployees = JSON.parse(saved);
+        this.employees = rawEmployees.map((emp) => {
+          if (emp.employeeType === "Manager") {
+            return new Manager(emp.name, emp.age, emp.payRate, emp.hours);
+          } else {
+            return new PartTime(emp.name, emp.age, emp.payRate, emp.hours);
+          }
+        });
+      }
     }
 
     // Display employee list
@@ -77,7 +99,7 @@
     // Add a new employee to the lise
     addEmployee() {
       const input = prompt(
-        "To add an employee, enter: name, age, pay rate, hours worked (separated by commas)"
+        "To add an employee to the list, enter: name, age, pay rate, hours worked (separated by commas)"
       );
 
       const [name, ageStr, payRateStr, hourStr] = input.split(",");
@@ -100,7 +122,9 @@
 
     // Remove an employee by ID
     removeEmployee() {
-      const input = prompt("Enter the ID of the employee to remove:");
+      const input = prompt(
+        "Enter the ID of the employee to remove them from the list"
+      );
       const id = parseInt(input);
 
       if (!isNaN(id) && id > 0 && id <= this.employees.length) {
@@ -108,14 +132,14 @@
         console.log("Employee has been removed.");
         this.displayEmployees();
       } else {
-        console.log("Invalid ID.");
+        console.log("Invalid ID Please Reenter The Employee Id.");
       }
     }
 
     // function that allows user to select from the menu
     menu() {
       const choice = prompt(
-        "Choose an option:\n1: Add New Employee\n2: Remove an Employee\n3: Show Employees\n4: Exit Menu"
+        "What would you like to accomplish:\n1: Add New Employee\n2: Remove an Employee\n3: Show Employees\n4: Exit Menu"
       );
 
       switch (choice) {
@@ -131,8 +155,6 @@
         case "4":
           console.log("Thank you!");
           return;
-        default:
-          console.log("Invalid option.");
       }
 
       // Loop back to the menu
